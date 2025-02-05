837a4228b366ee9a2362fc0564510c96b5beb44f
sahil <sharadjethva1311@gmail.com>
2025-02-05 10:57:22 +0530

    setup of primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { providePrimeNG } from 'primeng/config';
+import { MyPreset } from './assets/mytheme';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    providePrimeNG({
+      theme: {
+        preset: MyPreset,
+      }
+    })
+  ]
 };
